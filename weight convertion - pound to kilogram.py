2f050cf7feaa28f263034a2a01a31ed7ec3b6c51
Salmanabdullah <salmandcc@gmail.com>
2021-03-14 00:51:13 +0100

    Create weight convertion - pound to kilogram.py

---
+++ weight convertion - pound to kilogram.py
@@ -0,0 +1,4 @@
+# Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+weight = input("Enter your weight (in Pounds): ")
+weight_in_kilograms = int(weight) * 0.453592
+print("Your weight is:",weight_in_kilograms,"Kilograms")
