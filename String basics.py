66c493916b1c0b5f577a2a2157ae8ef304682218
Salmanabdullah <salmandcc@gmail.com>
2021-03-14 01:48:37 +0100

    Create String basics.py

---
+++ String basics.py
@@ -0,0 +1,18 @@
+# String basics
+# for multi line String we use triple quotes ''' '''
+text = '''
+Hi Nelly,
+
+This is a test email to you.
+
+Thank you
+Salman'''
+
+print(text)
+
+# to find the index 
+text = 'I wanna sleep. I\'m to tired' # \ backslash to avoid inverted comma
+print(text)
+print(text[0])
+print(text[0:3])
+print(text[1:-1])
